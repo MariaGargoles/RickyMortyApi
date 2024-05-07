a0843e10fe30245edb575e59a24ad01fe1dfe028
María Gárgoles <mariagargoles@live.com>
2024-05-07 12:00:03 +0200

    create routes and paths

---
+++ src/Modules/PersonCardComponent.jsx
@@ -0,0 +1,12 @@
+
+
+export const PersonCardComponent = (props) => {
+
+    return (
+        <>
+            <h3>{props.name}</h3>
+            <img src={props.image} alt={props.name} />
+            <p>{props.species}</p>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import App from './App.jsx';
+import { IndexPage } from './Pages/Index.jsx';
+import { PersonList } from './Pages/PersonList.jsx';
+import { EpisodeList } from './Pages/EpisodeList.jsx';
+import { Info } from './Pages/Info.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/personas" element={<PersonList />} />
+        <Route path="/episodios" element={<EpisodeList />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
