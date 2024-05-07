e967868e611fb7a6f219ecd95490462bb209579e
María Gárgoles <mariagargoles@live.com>
2024-05-07 14:54:17 +0200

    start fetch in react

--- src/Modules/PersonCardComponent/PersonCardComponent.jsx
+++ src/Modules/PersonCardComponent/PersonCardComponent.jsx
@@ -1,4 +1,4 @@
-
+import { PersonList } from "../../Pages/PersonList"
 
 export const PersonCardComponent = (props) => {
 
--- src/Pages/PersonList.jsx
+++ src/Pages/PersonList.jsx
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from 'react';
 
-export const PersonList = () => {
-    return
-    <>
-    
-    </>
-}
+export const PersonList = (props) => {
+    const [results, setResults] = useState([]);
+    const url = 'https://rickandmortyapi.com/api/character';
+
+    useEffect(() => {
+        const request = fetch(url)
+            .then(response => { 
+                if (response.ok){
+                    const jsonData = response.json();
+                    const arrayResults = jsonData.results.map(arrayResult => ({
+                        name: arrayResult.name,
+                        image: arrayResult.image,
+                        species: arrayResult.species
+                    }));
+                    setResults(arrayResults);
+                } else {
+                    console.log("error");
+                }
+            })
+            .catch(error => {
+                console.error('Hubo un problema con la llamada a la api:', error);
+            });
+    }, []);
+};
