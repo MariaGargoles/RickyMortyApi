ced0d7f1e28f1832c47db31dccf5a8578885d5b3
María Gárgoles <mariagargoles@live.com>
2024-05-12 13:35:00 +0200

    add taskslice

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -3,5 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 export const TaskSlice = createSlice({
   name: "task",
   initialState: [],
-  reducers: {},
+  reducers: {
+    addCharacter: (state, action) => {
+      state.push(action.payload);
+    },
+    removeCharacter: (state, action) => {},
+  },
 });
