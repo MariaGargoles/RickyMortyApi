f4f02b00e4b1a5afad14360520e63b69e6ca5de4
María Gárgoles <mariagargoles@live.com>
2024-05-14 10:57:28 +0200

    add api status, next step conver to redux api fetch

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -1,17 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { GetTaskListThunk } from "./taskThunk";
 
 export const TaskSlice = createSlice({
   name: "task",
-  initialState: [],
+  initialState: {
+    status: "idle",
+    data: [],
+    error: null,
+  },
   reducers: {
     addCharacter: (state, action) => {
-      state.push(action.payload);
+      state.data.push(action.payload);
     },
     removeCharacter: (state, action) => {
       return state.filter((character) => character.id !== action.payload);
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(GetTaskListThunk.pending, (state, action) => {
+        state.status = "pending";
+      })
+      .addCase(GetTaskListThunk.fulfilled, (state, action) => {
+        state.status = "fulfilled";
+      })
+      .addCase(GetTaskListThunk.rejected, (state, action) => {
+        state.status = "rejected";
+      });
+  },
 });
 
 export const { addCharacter, removeCharacter } = TaskSlice.actions;
